8ed95568b02a736539aaa7b42083629f0f5111ec
Izaz011 <izazsarkar11@gmail.com>
2022-11-09 10:32:02 +0530

    assignment axios

---
+++ src/controllers/practic.js
@@ -0,0 +1,27 @@
+
+
+// console.log(a())
+
+// function a(
+//     function (){
+//     console.log(5)
+//     }){
+// console.log(5)
+// }
+
+
+
+// // const b=function(x,function (y){}){
+
+
+// // }
+
+
+// var a;
+// console.log(a)// undefined
+// var a=10
+
+// console.log(a)// Cannot access 'a' before initialization
+// let a=10
+
+// console.log(a)//a is not defined
